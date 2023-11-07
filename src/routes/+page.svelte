2e4e3c19d039ce15eeb281ca25ac8c860d32f33a
Li Shen <lililogan@gmail.com>
2023-11-06 19:05:24 -0600

    p

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,18 @@
 </style>
 
 <div class="flex flex-col">
+
+    <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 bottom-8 left-0 border-b">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" class="flex items-center">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ABOUT US</span>
+        </a>
+        <div class="flex md:order-2">
+
+        </div>
+        </div>
+    </nav>
+  
     <div class="first-section flex flex-col justify-top items-end h-screen bg-green-800 p-24 justify-between">
         <div class="absolute left-24 top-24 text-[135px] font-black text-start text-white" style="line-height: 1;">
             <div>GREEN BY</div>
