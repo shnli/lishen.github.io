a089885e105b2323ee261b1a02d424a44e1dfce6
Li Shen <lililogan@gmail.com>
2023-11-06 18:03:54 -0600

    hello

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="js">
+</script>
+
+<div>
+    <div class="flex flex-col justify-top items-center h-screen bg-green-300 p-24">
+        <h1 class="text-5xl font-bold text-center">GREEN BY DESIGN</h1>
+        <div class="flex justify-between w-96 bg-white px-24 rounded-lg">
+            <p>About Us</p>
+            <p>SHOP</p>
+        </div>
+    </div>
+</div>
