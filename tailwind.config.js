a7845bf5aa24f073d81e6dc1edf879ba90b9fe9c
Li Shen <63427948+shnli@users.noreply.github.com>
2023-11-20 17:46:39 -0600

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
-  },
-  fonts: {
-    poppins: ['Poppins', 'sans-serif'],
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+        fp: ['Finger Paint', ...defaultTheme.fontFamily.sans]
+
+      }
+    },
   },
   plugins: [],
 }
